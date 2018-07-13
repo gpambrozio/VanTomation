03cace54e6fd7e9cebdf145da6d7f1f73d1f340b
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-07-12 22:07:33 -0600

    Very preliminary version of the app

---
+++ VanTomation/MasterManager.swift
@@ -0,0 +1,115 @@
+//
+//  MasterManager.swift
+//  VanTomation
+//
+//  Created by Gustavo Ambrozio on 7/2/18.
+//  Copyright © 2018 Gustavo Ambrozio. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+import BlueCapKit
+
+enum AppError: Error {
+    case invalidState
+    case resetting
+    case poweredOff
+    case unsupported
+    case unknown
+}
+
+public struct CommandCharacteristic: CharacteristicConfigurable {
+
+    // CharacteristicConfigurable
+    public static let uuid                                     = "12345679-1234-5678-1234-56789abc0010"
+    public static let name                                     = "Command"
+    public static let permissions: CBAttributePermissions      = [.readable, .writeable]
+    public static let properties: CBCharacteristicProperties   = [.read, .notify]
+    public static let initialValue                             = SerDe.serialize("0")
+
+}
+
+class MasterManager {
+
+    public static let shared = MasterManager()
+
+    enum Constants {
+        static let uuid = "12345678-1234-5678-1234-56789abc0010"
+    }
+
+    let manager = PeripheralManager(options: [CBPeripheralManagerOptionRestoreIdentifierKey: "us.gnos.BlueCap.peripheral-manager-example" as NSString])
+
+    let commandService = MutableService(uuid: Constants.uuid)
+
+    let commandCharacteristic = MutableCharacteristic(profile: StringCharacteristicProfile<CommandCharacteristic>())
+
+    private init() {
+        commandService.characteristics = [commandCharacteristic]
+        startAdvertising()
+    }
+
+    func startAdvertising() {
+        let uuid = CBUUID(string: Constants.uuid)
+
+        let startAdvertiseFuture = manager.whenStateChanges().flatMap { [unowned self] state -> Future<Void> in
+            switch state {
+            case .poweredOn:
+                self.manager.removeAllServices()
+                return self.manager.add(self.commandService)
+            case .poweredOff:
+                throw AppError.poweredOff
+            case .unauthorized:
+                throw AppError.invalidState
+            case .unsupported:
+                throw AppError.unsupported
+            case .resetting:
+                throw AppError.resetting
+            case .unknown:
+                throw AppError.unknown
+            }
+        }.flatMap { [unowned self] _ -> Future<Void> in
+            self.manager.startAdvertising("VanTomation", uuids: [uuid])
+        }
+
+        startAdvertiseFuture.onSuccess { _ in
+            self.commandCharacteristic.value = SerDe.serialize("-1")
+        }
+
+        startAdvertiseFuture.onFailure { [weak self] error in
+            guard let `self` = self else { return }
+            print("Error: \(error)")
+
+            switch error {
+            case AppError.unsupported,
+                 AppError.unknown:
+                break
+            default:
+                _ = self.manager.stopAdvertising()
+                DispatchQueue.main.async {
+                    self.manager.reset()
+                }
+            }
+        }
+
+        let accelerometerUpdatePeriodFuture = startAdvertiseFuture.flatMap { [unowned self] in
+            self.commandCharacteristic.startRespondingToWriteRequests()
+        }
+        accelerometerUpdatePeriodFuture.onSuccess { [unowned self] (request, _) in
+            guard let value = request.value, value.count > 0 && value.count <= 8 else {
+                self.commandCharacteristic.respondToRequest(request, withResult:CBATTError.invalidAttributeValueLength)
+                return
+            }
+            self.commandCharacteristic.value = value
+            self.commandCharacteristic.respondToRequest(request, withResult:CBATTError.success)
+        }
+    }
+
+    func send(command: String) {
+        do {
+            try self.commandCharacteristic.update(withData: command.data(using: .utf8)!)
+        } catch let e {
+            print("Exception \(e)")
+        }
+    }
+}
