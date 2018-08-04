d4a88aaf3320d779b01f09e1a4a2632be4168ee0
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-08-03 22:13:05 -0600

    Adding a script to make it easier to scan for devices and services

---
+++ pi/scan.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+from bluepy.btle import Scanner, DefaultDelegate, BTLEException
+from bluepy import btle
+import uuid
+import time
+import binascii
+import traceback
+import os
+
+if os.getenv('C', '1') == '0':
+    ANSI_RED = ''
+    ANSI_GREEN = ''
+    ANSI_YELLOW = ''
+    ANSI_CYAN = ''
+    ANSI_WHITE = ''
+    ANSI_OFF = ''
+else:
+    ANSI_CSI = "\033["
+    ANSI_RED = ANSI_CSI + '31m'
+    ANSI_GREEN = ANSI_CSI + '32m'
+    ANSI_YELLOW = ANSI_CSI + '33m'
+    ANSI_CYAN = ANSI_CSI + '36m'
+    ANSI_WHITE = ANSI_CSI + '37m'
+    ANSI_OFF = ANSI_CSI + '0m'
+
+def reverse_uuid(service_uuid):
+    return uuid.UUID(bytes="".join([uuid.UUID(service_uuid).bytes[15-i] for i in range(16)])).hex
+
+def dump_services(dev):
+    services = sorted(dev.services, key=lambda s: s.hndStart)
+    for s in services:
+        print ("\t%04x: %s" % (s.hndStart, s))
+        if s.hndStart == s.hndEnd:
+            continue
+        chars = s.getCharacteristics()
+        for i, c in enumerate(chars):
+            props = c.propertiesToString()
+            h = c.getHandle()
+            if 'READ' in props:
+                val = c.read()
+                if c.uuid == btle.AssignedNumbers.device_name:
+                    string = ANSI_CYAN + '\'' + \
+                        val.decode('utf-8') + '\'' + ANSI_OFF
+                elif c.uuid == btle.AssignedNumbers.device_information:
+                    string = repr(val)
+                else:
+                    string = '<s' + binascii.b2a_hex(val).decode('utf-8') + '>'
+            else:
+                string = ''
+            print ("\t%04x:    %-59s %-12s %s" % (h, c, props, string))
+
+            while True:
+                h += 1
+                if h > s.hndEnd or (i < len(chars) - 1 and h >= chars[i + 1].getHandle() - 1):
+                    break
+                try:
+                    val = dev.readCharacteristic(h)
+                    print ("\t%04x:     <%s>" %
+                           (h, binascii.b2a_hex(val).decode('utf-8')))
+                except btle.BTLEException:
+                    break
+
+
+scanner = Scanner()
+print("Starting scan")
+while True:
+    devices = scanner.scan(5)
+    for dev in devices:
+        scan_data = dev.getScanData()
+        services = [s[2] for s in scan_data if s[0] in [3, 6, 7]]
+
+        print("Found device %s (%s) services %s" % (dev.addr, dev.getValueText(9), scan_data))
+
+        interested_uuid = "1918"
+        if interested_uuid in services:
+            peripheral = btle.Peripheral()
+            peripheral.connect(dev)
+
+            dump_services(peripheral)
+            peripheral.disconnect()
+            break
