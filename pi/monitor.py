fa9817b442a78df849659a09d5b75df6e87ca399
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-09-07 00:10:54 -0700

    Better layout

--- pi/monitor.py
+++ pi/monitor.py
@@ -25,26 +25,28 @@ root.title("VanTomation")
 lines = [
     ["Temperature", "%.1f F", 1.0],
     ["Humidity", "%.1f %%", 1.0],
-    ["Heading", "%d", 1],
     ["Speed", "%d mph", 1],
+    ["Heading", "%d", 1],
     ["Altitude", "%d ft", 3.281],
     ["Altitude", "%d m", 1.0],
 ]
 
-title = tk.Label(root, text="Hello NonVanLifers!", font="Helvetica 24")
-title.pack()
+title = tk.Label(root, text="Hello NonVanLifers!", font="Helvetica 30")
+title.grid(row=0, columnspan=2)
 spacer = tk.Label(root, text=" ", font="Helvetica 10")
-spacer.pack()
+spacer.grid(row=1, columnspan=2)
 
-ui = [tk.Label(root, text=line[0] + ": ?", font="Helvetica 18") for line in lines]
-for element in ui:
-    element.pack()
+ui = [tk.Label(root, text="", font="Helvetica 24") for line in lines]
+for (n, line) in enumerate(lines):
+    title = tk.Label(root, text=line[0], font="Helvetica 24")
+    title.grid(row=n+2, column=0, sticky=tk.W)
+    ui[n].grid(row=n+2, column=1, sticky=tk.W)
 
 def reload():
     read_state()
     for (n, line) in enumerate(lines):
         if line[0] in state:
-            ui[n].config(text=("%s: " + line[1]) % (line[0], line[2] * state[line[0]]["value"]))
+            ui[n].config(text=line[1] % (line[2] * state[line[0]]["value"]))
     root.after(1000, reload)
 
 reload()
